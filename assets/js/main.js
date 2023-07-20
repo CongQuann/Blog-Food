8460eeacaf6f3e3abf843d4052f39cefffb9dfbf
CongQuann <quanpt1610@gmail.com>
2023-07-20 22:37:55 +0700

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ var counter=1;
 var body = document.getElementById("sticky");
 window.onscroll= function(){
     console.info(document.documentElement.scrollTop);
-    if(document.documentElement.scrollTop < 780){
+    if(document.documentElement.scrollTop < 789){
         body.style.position ="fixed";
         body.style.marginLeft = "68.4%";
         body.style.width = "30%";
@@ -22,7 +22,7 @@ window.onscroll= function(){
         body.style.position ="absolute";
         body.style.marginLeft = "68.4%";
         body.style.width = "30%";
-        body.style.marginTop = "57%";
+        body.style.marginTop = "54%";
         // body.style.backgroundColor = "var(--primary-color)";
         body.style.zIndex = 9;
     }
