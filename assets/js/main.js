946ccca56a2695a798ace30dd71b35def1d5562d
CongQuann <quanpt1610@gmail.com>
2023-07-30 01:35:25 +0700

    add event Go to top

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,21 +20,20 @@ var counter=1;
           counter=1;
       }
   },3000);
-var body = document.getElementById("sticky");
-// window.onscroll= function(){
-//     console.info(document.documentElement.scrollTop);
-//     if(document.documentElement.scrollTop < 789){
-//         body.style.position ="fixed";
-//         body.style.marginLeft = "68.4%";
-//         body.style.width = "30%";
-//         body.style.marginTop = "0px";
-//         // body.style.backgroundColor = "var(--primary-color)";
-//     }
-//     else{
-//         body.style.position ="absolute";
-//         body.style.marginLeft = "68.4%";
-//         body.style.width = "30%";
-//         body.style.marginTop = "54%";
-//         // body.style.backgroundColor = "var(--primary-color)";
-//     }
-// }
+var gototop = document.getElementById("Gototop");
+ window.onscroll= function(){
+    console.info(document.documentElement.scrollTop);
+     if(document.documentElement.scrollTop > 100){
+         gototop.style.display = "block";
+     }
+     else{
+        gototop.style.display = "none";
+     }
+ }
+ function goToTop(){
+    var x= setInterval(function(){
+        document.documentElement.scrollTop-=20;
+        if(document.documentElement.scrollTop<=0)
+        clearInterval(x);
+    },1)
+ }
