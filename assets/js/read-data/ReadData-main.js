bb6905d94710f1e9fae3959799cb1f0038d66d65
CongQuann <quanpt1610@gmail.com>
2023-08-10 00:50:13 +0700

    add data main page

--- assets/js/read-data/ReadData-main.js
+++ assets/js/read-data/ReadData-main.js
@@ -1,6 +1,6 @@
 //read data
  function loadCates(){
-     fetch("./assets/data/data-danhmuc-index.json").then(res => res.json()).then(data => {
+     fetch("./assets/data/Data-MainPage/data-danhmuc-index.json").then(res => res.json()).then(data => {
          let h = "";
          for(let c of  data)
          h +=`<li><a href="${c.href}">${c.name}<i class="${c.class}"></i></a></li>`;
@@ -10,7 +10,7 @@
      })
  }
  function loadThongBao(){
-     fetch("./assets/data/data-thongbao-index.json").then(res => res.json()).then(data => {
+     fetch("./assets/data/Data-MainPage/data-thongbao-index.json").then(res => res.json()).then(data => {
          let h = "";
          for(let c of  data)
          h +=`<li class="${c.class_li}">
@@ -29,7 +29,7 @@
      })
  }
  function loadAutoImg(){
-    fetch("./assets/data/data-AutoImg-index.json").then(res => res.json()).then(data => {
+    fetch("./assets/data/Data-MainPage/data-AutoImg-index.json").then(res => res.json()).then(data => {
         let h = "";
         for(let c of  data)
         h +=`
@@ -46,9 +46,164 @@
         e.insertAdjacentHTML("afterend",h);
     })
 }
+
+function loadNewPost(){
+    fetch("./assets/data/Data-MainPage/data-NewPost-index.json").then(res => res.json()).then(data => {
+        let h = "";
+        for(let c of  data)
+        h +=`
+        <div class="baiviet">
+            <a href="#">
+                <div class="img"><img src="./assets/images/${c.img}" alt="${c.alt}"></div>
+                <div class="description-baiviet">
+                    <h3 id="item-search">${c.title}</h3>
+                    <p>${c.content}</p>
+                </div>
+            </a>
+        </div>
+            `;
+            let e = document.getElementById("child-right")
+            if(e !== null)
+            e.innerHTML += h;
+    })
+}
+function loadRandomPost(){
+    fetch("./assets/data/Data-MainPage/data-RandomPost-index.json").then(res => res.json()).then(data => {
+        let h = "";
+        for(let c of  data)
+        h +=`
+        <div class="content">
+            <div class="img">
+                <a href="#">
+                    <img src="./assets/images/${c.img}" alt="${c.alt}">
+                </a>
+            </div>
+            <div class="description-content">
+                <a href="#">
+                    <h3 id="item-search">${c.title}</h3>
+                </a>
+                <p>${c.content}</p>
+                <a href="#">
+                    <p class="XemThem">Xem thêm</p>
+                </a>
+                <p>${c.date}</p>
+            </div>
+        </div>
+            `;
+            let e = document.getElementById("child-random")
+            if(e !== null)
+            e.innerHTML += h;
+    })
+}
+function loadFoodAndArt(){
+    fetch("./assets/data/Data-MainPage/data-FoodAndArt-index.json").then(res => res.json()).then(data => {
+        let h = "";
+        for(let c of  data)
+        h +=`
+            <div class="count">
+                <a href="#">
+                    <p class="country" id="item-search">${c.country}</p>
+                    <p class="title-fcountry" id="item-search">${c.title}</p>
+                    <p class="introduce-fcountry">${c.introduce}</p>
+                </a>
+            </div>
+            <hr>
+            `;
+            let e = document.getElementById("art")
+            if(e !== null)
+            e.innerHTML += h;
+    })
+}
+function loadPostFireFood(){
+    fetch("./assets/data/Data-MainPage/data-FiredFood-index.json").then(res => res.json()).then(data => {
+        let h = "";
+        for(let c of  data)
+        h +=`
+        <div class="post">
+            <a href="#">
+                <div class="img-post">
+                    <img src="assets/images/${c.img}" alt="${c.alt}">
+                    <div class="title-post">${c.title}</div>
+                </div>
+                <div class="content-post">
+                    <h4 id="item-search">${c.name_food}</h4>
+                    <div class="rating">
+                        <span>${c.rate}</span>
+                        <div class="star">${c.star}</div>
+                    </div>
+                    <p>${c.content}</p>
+                </div>
+            </a>
+        </div>
+            `;
+            let e = document.getElementById("frame-posts-FiredFood")
+            if(e !== null)
+            e.innerHTML += h;
+    })
+}
+function loadPostSimpleFood(){
+    fetch("./assets/data/Data-MainPage/data-SimpleFood-index.json").then(res => res.json()).then(data => {
+        let h = "";
+        for(let c of  data)
+        h +=`
+        <div class="post">
+            <a href="#">
+                <div class="img-post">
+                    <img src="assets/images/${c.img}" alt="${c.alt}">
+                    <div class="title-post">${c.title}</div>
+                </div>
+                <div class="content-post">
+                    <h4 id="item-search">${c.name_food}</h4>
+                    <div class="rating">
+                        <span>${c.rate}</span>
+                        <div class="star">${c.star}</div>
+                    </div>
+                    <p>${c.content}</p>
+                </div>
+            </a>
+        </div>
+            `;
+            let e = document.getElementById("frame-posts-SimpleFood")
+            if(e !== null)
+            e.innerHTML += h;
+    })
+}
+function loadPosthealthy(){
+    fetch("./assets/data/Data-MainPage/data-healthy-index.json").then(res => res.json()).then(data => {
+        let h = "";
+        for(let c of  data)
+        h +=`
+        <div class="post">
+            <a href="#">
+                <div class="img-post">
+                    <img src="assets/images/${c.img}" alt="${c.alt}">
+                    <div class="title-post">${c.title}</div>
+                </div>
+                <div class="content-post">
+                    <h4 id="item-search">${c.name_food}</h4>
+                    <div class="rating">
+                        <span>${c.rate}</span>
+                        <div class="star">${c.star}</div>
+                    </div>
+                    <p>${c.content}</p>
+                </div>
+            </a>
+        </div>
+            `;
+            let e = document.getElementById("frame-posts-healthy")
+            if(e !== null)
+            e.innerHTML += h;
+    })
+}
  window.onload = function(){
      loadCates();
      loadThongBao();
     loadAutoImg();
+    loadNewPost();
+    loadRandomPost();
+    loadFoodAndArt();
+    loadPostFireFood();
+    loadPostSimpleFood();
+    loadPosthealthy();
  }
 //end read data
